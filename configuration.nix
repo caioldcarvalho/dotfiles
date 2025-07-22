4e9c8459dba537c3a2c6ebf0deff4d77df4dee5f
Caio Carvalho <caioldcarvalho@protonmail.com>
2025-07-22 11:08:03 -0300

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -1,35 +1,20 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-  # Bootloader.
+  # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/Sao_Paulo";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "pt_BR.UTF-8";
     LC_IDENTIFICATION = "pt_BR.UTF-8";
@@ -42,26 +27,19 @@
     LC_TIME = "pt_BR.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "br";
-    variant = "";
+  # X11 + GDM + GNOME
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+    xkb.layout = "br";
+    xkb.variant = "";
   };
 
-  # Configure console keymap
   console.keyMap = "br-abnt2";
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   security.polkit.enable = true;
@@ -71,77 +49,87 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # User
   users.users.caio = {
     isNormalUser = true;
     description = "caio";
-    extraGroups = [ "networkmanager" "wheel" "docker"];
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
     shell = pkgs.fish;
-    packages = with pkgs; [
-    #  thunderbird
-    ];
+    packages = with pkgs; [ ];
   };
 
-  programs.dconf.enable = true;
-  programs.hyprlock.enable = true;
+  # Programas habilitados
+  programs = {
+    dconf.enable = true;
+    fish.enable = true;
+    firefox.enable = true;
+    starship.enable = true;
+    hyprlock.enable = true;
+  };
 
-
+  # Hyprland
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
   };
 
-  programs.fish.enable = true;
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # Pacotes do sistema
   environment.systemPackages = with pkgs; [
-    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    # Editors
+    neovim
+    vscode
+
+    # CLI & Network
+    git
     wget
     curl
-    git
-    chromium
-    vscode
-    obsidian
     postman
-    kitty
-    fish
-    fishPlugins.tide
-
+    tree
     openssl
 
-    nerd-fonts.iosevka
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.victor-mono
-    nerd-fonts.zed-mono
-    nerd-fonts.ubuntu
-    tela-icon-theme
-
-    # Terminal useful and useless shit
-    tree
+    # Terminal & Shell
+    fish
+    starship
+    kitty
     neofetch
     pfetch
     cava
     cbonsai
 
-    # functional shit for hyprland
+    # DevOps / Kubernetes
+    kubectl
+    kind
+    kubernetes-helm
+    lens
+
+    # Pessoal
+    obsidian
+    slack
+    clickup
+
+    # Browsers
+    chromium
+
+    # Fontes
+    nerd-fonts.lilex
+    nerd-fonts.iosevka
+    nerd-fonts.geist-mono
+    nerd-fonts.victor-mono
+    nerd-fonts.d2coding
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.zed-mono
+    nerd-fonts.ubuntu
+
+    # Ícones
+    tela-icon-theme
+
+    # GNOME tweaks e extensões
+    gnome-tweaks
+    gnomeExtensions.user-themes
+
+    # Hyprland tools
     xdg-desktop-portal-hyprland
     xdg-desktop-portal
     waybar
@@ -157,42 +145,34 @@
     playerctl
     pulseaudio
     swww
-
   ];
 
   environment.variables.GTK_ICON_THEME = "Tela";
 
-  virtualisation.docker.rootless = {
+  fonts.packages = with pkgs; [
+    nerd-fonts.lilex
+    nerd-fonts.geist-mono
+    nerd-fonts.victor-mono
+    nerd-fonts.iosevka
+    nerd-fonts.d2coding
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.zed-mono
+    nerd-fonts.ubuntu
+  ];
+
+  # Docker
+  virtualisation.docker = {
     enable = true;
-    setSocketVariable = true;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
   };
 
+  # Unfree
+  nixpkgs.config.allowUnfree = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
-
+  # Versão
+  system.stateVersion = "24.11";
 }
+
